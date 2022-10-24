9bc3890cd17359626e036cc7fb1724699aa584a5
Daniil <d.kirpa@webpractik.ru>
2022-10-24 10:43:12 +0300

    Тестовое задание содержащее абстрактный класс человека,интерфейс примата,трейт офисного планктона и класс разработчика

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 
-require 'class.php';
+namespace App;
 
-
-    printf('повышения бэкендера с ');
+printf('повышения бэкендера с ');
     $backend = new Dev(1);
     printf($backend->show()."\n");
     $backend->Up();
@@ -13,26 +12,24 @@ require 'class.php';
 
     echo '|бэкендер хочет есть->';
 
-    $backend->eat($e=1);
+    $backend->eat($e = 1);
 
     printf('|бахнул дебаф офисного планктона    ');
     $backend->cofeeTime($cof);
 
-?>
-<br>
-<?php
+    echo '<br>';
     echo 'фронтендер уровня  ';
         $frontend = new dev(1);
     echo $frontend->show()."\n";
 
     echo '|фронтендер хочет пить->';
-    $backend->drink($d=2);
+    $backend->drink($d = 2);
 //     $frontend->work(2);
 
 echo '<br>';
-$dev1 = new Dev();
+$dev1 = new Dev(2);
 
-$dev2 = new Dev();
+$dev2 = new Dev(1);
 $dev1->work($dev2);
 
 ?>
--- mail.php
+++ mail.php
@@ -1,22 +1,31 @@
-<?php
-class person{
-    var $name;
-    var $color;
-    var $sex;
-    var $age;
-    function __construct($name,$age='',$sex='boy'){
+<?php declare(strict_types=1);
+class mail
+{
+    public $name;
+    public $color;
+    public $sex;
+    public $age;
+
+    public function __construct($name, $age = '', $sex = 'boy')
+    {
         $this->name = $name;
         $this->age = $age;
         $this->sex = $sex;
         $this->color = 'yello';
     }
-    function eat(){
+
+    public function eat(): void
+    {
         echo $this->name.'Хочу съесть';
     }
-    function xinxi(){
+
+    public function xinxi(): void
+    {
         echo $this->name.' is '.$this->sex.' and age is '.$this->age.' fuse is '.$this->color;
     }
-    function zuoyong(){
+
+    public function zuoyong(): void
+    {
         // Подобно этому внутреннему вызову, это эквивалентно введению кода eat () в zuoyong () вместо перехода к eat () для продолжения выполнения
         // Если это URL типа http: // localhost / zuoyong? Food = xigua для вызова zuoyong ()
         // Затем eat () может напрямую получить параметр url через $ _GET ['food'], потому что внутри функции можно использовать глобальные переменные
@@ -24,10 +33,10 @@ class person{
     }
 }
 
-$son = new person('cuihua',25,'girl');// Передаваемый здесь параметр должен соответствовать порядку параметров в методе построения класса
-//$son->xinxi();//cuihua is girl and age is 25 fuse is yello
+$son = new person('kto-to', 25, 'girl'); // Передаваемый здесь параметр должен соответствовать порядку параметров в методе построения класса
+// $son->xinxi();//cuihua is girl and age is 25 fuse is yello
 $son->name = 'Тиан Ню';
-$son->eat();// Тянь Ню хочет поесть
+$son->eat(); // Тянь Ню хочет поесть
 ?>
 <!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
 <form enctype="multipart/form-data" action="__URL__" method="POST">
